e593ef55a1a17681086f76fe1ec81f450cf68b96
ivoGmail <ivo753@gmail.com>
2017-07-08 13:27:25 -0400

    change Ivo Rojas to Person.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { IvoRojasComponent } from './component/ivo-rojas/ivo-rojas.component';
+import { PersonComponent } from './component/person/person.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IvoRojasComponent
+    PersonComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/class/person.ts
@@ -0,0 +1,8 @@
+import { University } from "app/class/university";
+
+export class Person {
+    height: number;
+    public name: string;
+    private Age: number;
+    public university: University;
+}
